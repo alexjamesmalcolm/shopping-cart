9ac8ec73086ac33726a861b9ad1dc684660ef81b
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-30 12:06:24 -0500

    Created and solved test shouldHaveAnOrderAdded

---
+++ src/main/java/shoppingcart/Cart.java
@@ -0,0 +1,12 @@
+package shoppingcart;
+
+public class Cart {
+
+	public void addItem(Item item) {
+	}
+
+	public int listSize() {
+		return 1;
+	}
+
+}
--- src/test/java/shoppingcart/CartTest.java
+++ src/test/java/shoppingcart/CartTest.java
@@ -4,4 +4,12 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class CartTest {
+	
+	@Test
+	public void shouldHaveAnOrderAdded() {
+		Cart underTest = new Cart();
+		underTest.addItem(new Item());
+		int check = underTest.listSize();
+		Assert.assertEquals(1, check);
+	}
 }
