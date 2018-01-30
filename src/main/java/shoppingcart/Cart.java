5b421ef0ffbda1936b7bb39e85b86f8b42e3b992
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-30 13:12:43 -0500

    Created and solved test shouldHaveTwoOrdersAdded

--- src/main/java/shoppingcart/Cart.java
+++ src/main/java/shoppingcart/Cart.java
@@ -1,12 +1,18 @@
 package shoppingcart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
 
+	private List<Item> items = new ArrayList<Item>();
+
 	public void addItem(Item item) {
+		items.add(item);
 	}
 
 	public int listSize() {
-		return 1;
+		return items.size();
 	}
 
 }
--- src/test/java/shoppingcart/CartTest.java
+++ src/test/java/shoppingcart/CartTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class CartTest {
-	
+
 	@Test
 	public void shouldHaveAnOrderAdded() {
 		Cart underTest = new Cart();
-		underTest.addItem(new Item());
+		underTest.addItem(new Item("oranges", 0.99, 3));
 		int check = underTest.listSize();
 		Assert.assertEquals(1, check);
+	}
+
+	@Test
+	public void shouldHaveTwoOrdersAdded() {
+		Cart underTest = new Cart();
+		underTest.addItem(new Item());
+		underTest.addItem(new Item());
+		int check = underTest.listSize();
+		Assert.assertEquals(2, check);
 	}
 }
